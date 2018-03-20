c14ff9af7594bcedf0f814ed211cbd06f2329dc3
AlexKreuz <37532231+AlexKreuz@users.noreply.github.com>
2018-03-20 15:03:11 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const TelegramBot = require ('node-telegram-bot-api');
-
+const fs = require ('fs')
 const TOKEN = '479065680:AAHUysRRFisfZdfEEPgM-uBvcWrrw0fYcRc';
-
-const bot = new TelegramBot(TOKEN, {
-    polling: true
-});
+const _ = require ('lodash')
+const bot = new TelegramBot(TOKEN, {polling: true})
 const ANSWER1 = {
     good: 'Хорошо',
     bad: 'Плохо',
@@ -12,20 +10,27 @@ const ANSWER1 = {
     dog: 'Щеночек',
     back: 'Назад'
 }
+const PicScrs = {
+    [ANSWER1.cat]: [
+        'cat1.jpg',
+        'cat2.jpg',
+        'cat3.jpg'
+    ],
+    [ANSWER1.dog]: [
+        'dog1.jpg',
+        'dog2.jpg',
+        'dog3.jpg'
+    ]
+}
+
+
+
+
 bot.onText(/\/start/, msg => {
-    const text =  `Привет, ${msg.from.first_name}\nКак дела?`;
+    Greeting(msg)
+})
 
-    bot.sendMessage(msg.chat.id, text, {
-        reply_markup: {
-            keyboard: [
-                [ANSWER1.good, ANSWER1.bad]
-        ]
-        }
-    })
-
-});
-
-bot.on('message',msg => {
+bot.on('message', msg => {
     switch (msg.text) {
         case ANSWER1.bad:
             sendPictureScreen(msg.chat.id)
@@ -33,16 +38,11 @@ bot.on('message',msg => {
         case ANSWER1.good:
             break
         case ANSWER1.back:
-            bot.sendMessage(msg.chat.id, 'Так как ваше настроение?',{
-            reply_markup: {
-                keyboard: [
-                    [ANSWER1.good, ANSWER1.bad]
-                ]
-            }
-        })
+            Greeting(msg, false)
             break
         case ANSWER1.dog:
         case ANSWER1.cat:
+            sendPictureByName(msg.chat.id, msg.text)
             break
 
     }
@@ -57,4 +57,36 @@ function sendPictureScreen(chatId) {
             ]
         }
     })
+}
+
+function Greeting(msg, Hello = true) {
+    const text = Hello
+        ? `Привет, ${msg.from.first_name}\nКак настроение?`
+        : `Как настроение?`
+
+    bot.sendMessage(msg.chat.id, text, {
+        reply_markup: {
+            keyboard: [
+                [ANSWER1.good, ANSWER1.bad]
+            ]
+        }
+    })
+}
+
+function sendPictureByName (chatId, picName) {
+    const scrs = PicScrs[picName]
+
+    const scr = scrs [_.random(0, scrs.length -1)]
+
+    bot.sendMessage(chatId, `Так...посмотрим, что тут у нас...`)
+
+
+
+fs.readFile(`${__dirname}/pictures/${scr}`, (error, picture) => {
+    if (error) throw new Error(error)
+
+    bot.sendPhoto(chatId, picture).then(() =>{
+        bot.sendMessage(chatId, `Ну как вам? Показать кого-нибудь ещё?`)
+    })
+})
 }
